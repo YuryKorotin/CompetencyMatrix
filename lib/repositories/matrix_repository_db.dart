d89b593608ce40d2cd42bf0244a11fb312f2ddff
YuryKorotin <yurykorotin73@gmail.com>
2019-03-28 23:01:42 +0400

    Add sql requests

--- lib/repositories/matrix_repository_db.dart
+++ lib/repositories/matrix_repository_db.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'dart:io' as io;
+import 'package:competency_matrix/database/models/knowledge_db.dart';
+import 'package:competency_matrix/database/models/level_db.dart';
 import 'package:competency_matrix/database/models/matrix_db.dart';
+import 'package:competency_matrix/database/models/matrix_detaildb.dart';
 import 'package:competency_matrix/utils/consts.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
@@ -10,12 +13,15 @@ class MatrixRepositoryDb{
   static const String MATRIX_TABLE_NAME = "matrices";
   static const String MATRIX_DETAIL_TABLE_NAME = "matrix_details";
   static const String KNOWLEDGE_ITEMS_TABLE_NAME = "knowlede_items";
+  static const String LEVELS_TABLE_NAME = "levels";
 
   static const String ID_COLUMN_NAME = "id";
   static const String NAME_COLUMN_NAME = "name";
   static const String CATEGORY_COLUMN_NAME = "category";
   static const String DESCRITPION_COLUMN_NAME = "description";
 
+  static const String MATRIX_ID_COLUMN_NAME = "matrix_id";
+  static const String KNOWLEDGE_ITEM_ID_COLUMN_NAME = "knowledge_item_id";
 
   static Database _db;
 
@@ -38,8 +44,31 @@ class MatrixRepositoryDb{
   void _onCreate(Database db, int version) async {
     // When creating the db, create the table
     await db.execute(
-        "CREATE TABLE $MATRIX_TABLE_NAME($ID_COLUMN_NAME INTEGER PRIMARY KEY, $NAME_COLUMN_NAME TEXT, $CATEGORY_COLUMN_NAME TEXT, $DESCRITPION_COLUMN_NAME TEXT)");
-    print("Created tables");
+        """CREATE TABLE $MATRIX_TABLE_NAME
+        ($ID_COLUMN_NAME INTEGER PRIMARY KEY, 
+        $NAME_COLUMN_NAME TEXT, 
+        $CATEGORY_COLUMN_NAME TEXT, 
+        $DESCRITPION_COLUMN_NAME TEXT)
+        """);
+
+    await db.execute(
+        """CREATE TABLE $KNOWLEDGE_ITEMS_TABLE_NAME
+        ($ID_COLUMN_NAME INTEGER PRIMARY KEY, 
+        $NAME_COLUMN_NAME TEXT,
+        $MATRIX_ID_COLUMN_NAME INTEGER NOT NULL,
+        FOREIGN KEY ($MATRIX_ID_COLUMN_NAME) 
+        REFERENCES $MATRIX_TABLE_NAME($ID_COLUMN_NAME)
+        )""");
+
+    await db.execute(
+        """CREATE TABLE $LEVELS_TABLE_NAME
+        ($ID_COLUMN_NAME INTEGER PRIMARY KEY, 
+        $NAME_COLUMN_NAME TEXT, 
+        $DESCRITPION_COLUMN_NAME TEXT
+        $KNOWLEDGE_ITEM_ID_COLUMN_NAME INTEGER NOT NULL,
+        FOREIGN KEY ($KNOWLEDGE_ITEM_ID_COLUMN_NAME) 
+        REFERENCES $KNOWLEDGE_ITEMS_TABLE_NAME($ID_COLUMN_NAME)
+        )""");
   }
 
   // Retrieving matrices from Matrix Tables
@@ -58,6 +87,49 @@ class MatrixRepositoryDb{
               progress: 0));
     }
     return matrices;
+  }
+
+  Future<List<LevelDb>> getLevels(BigInt knowledgeItemId) async {
+    var dbClient = await db;
+    List<Map> list =
+    await dbClient.rawQuery(
+        """SELECT * FROM $LEVELS_TABLE_NAME 
+        WHERE $KNOWLEDGE_ITEM_ID_COLUMN_NAME=$knowledgeItemId""");
+    List<LevelDb> levels = new List();
+    for (int i = 0; i < list.length; i++) {
+      levels.add(
+          new LevelDb(
+              id: BigInt.from(list[i][ID_COLUMN_NAME]),
+              name: list[i][NAME_COLUMN_NAME],
+              description: list[i][DESCRITPION_COLUMN_NAME]
+          ));
+    }
+    return levels;
+  }
+
+  Future<List<KnowledgeItemDb>> getKnowledgeItems(BigInt matrixId) async {
+    var dbClient = await db;
+    List<Map> list = await dbClient.rawQuery('SELECT * FROM $KNOWLEDGE_ITEMS_TABLE_NAME WHERE $MATRIX_ID_COLUMN_NAME=$matrixId');
+    List<KnowledgeItemDb> knowledgeItems = new List();
+    for (int i = 0; i < list.length; i++) {
+      knowledgeItems.add(
+          new KnowledgeItemDb(
+              id: BigInt.from(list[i][ID_COLUMN_NAME]),
+              name: list[i][NAME_COLUMN_NAME],
+              levelDbItems: await getLevels(BigInt.from(list[i][ID_COLUMN_NAME]))));
+  }
+    return knowledgeItems;
+  }
+
+  Future<MatrixDetailDb> getMatrix(BigInt id) async {
+    var dbClient = await db;
+    List<Map> list = await dbClient.rawQuery('SELECT * FROM $MATRIX_TABLE_NAME WHERE $ID_COLUMN_NAME=$id');
+    MatrixDetailDb matrix = new MatrixDetailDb(
+      id: BigInt.from(list[0][ID_COLUMN_NAME]),
+      name: list[0][NAME_COLUMN_NAME],
+      knowledgeDbItems: await getKnowledgeItems(BigInt.from(list[0][ID_COLUMN_NAME])));
+
+    return matrix;
   }
 
   void saveMatrix(MatrixDb matrix) async {
