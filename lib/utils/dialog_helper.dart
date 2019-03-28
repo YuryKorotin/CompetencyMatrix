250720622e7f584389af0eba1f10f673a6b902f4
YuryKorotin <yurykorotin73@gmail.com>
2019-03-28 14:23:43 +0400

    Add dialog helper

---
+++ lib/utils/dialog_helper.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class DialogHelper {
+  void showQuestionDialog(
+      BuildContext context,
+      VoidCallback resultAction,
+      String title,
+      String content) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text(title),
+          content: new Text(content),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Yes"),
+              onPressed: () {
+                resultAction();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
